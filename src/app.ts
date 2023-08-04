9dafd62aaffbfad90706e157b00273868e56eb14
fedya <wrs22n@gmail.com>
2023-08-04 18:23:25 +0300

    complete-task/2

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import { TodoList } from "./todo.js";
+import { TodoList } from './todo.js';
 
 const todoList = new TodoList();
 
 todoList.renderTodoList();
 
-todoList.addItemBtn.addEventListener("click", (e: Event) => {
-    e.preventDefault();
+todoList.addItemBtn.addEventListener('click', (e: Event) => {
+  e.preventDefault();
 
-    if (todoList.addItemText.value.trim() === "") {
-        return;
-    }
+  if (todoList.addItemText.value.trim() === '') {
+    return;
+  }
 
-    const newTodo = {
-        text: todoList.addItemText.value,
-        expDate: Date.now(),
-        id: Date.now().toString(),
-    };
+  const newTodo = {
+    text: todoList.addItemText.value,
+    expDate: Date.now(),
+    id: Date.now().toString(),
+  };
 
-    todoList.todos.push(newTodo);
-    todoList.refreshList();
+  todoList.todos.push(newTodo);
+  todoList.refreshList();
 
-    todoList.addItemText.value = "";
+  todoList.addItemText.value = '';
 });
 
-todoList.clearAll.addEventListener("click", (e) => {
-    e.preventDefault();
-    todoList.todos = [];
-    todoList.refreshList();
+todoList.clearAll.addEventListener('click', (e) => {
+  e.preventDefault();
+  todoList.todos = [];
+  todoList.refreshList();
 });
--- src/todo.ts
+++ src/todo.ts
@@ -1,138 +1,136 @@
 interface ITodo {
-    text: string;
-    expDate: number;
-    id: string;
+  text: string;
+  expDate: number;
+  id: string;
 }
 
 export class TodoList {
-    public addItemText: HTMLInputElement;
-    public addItemBtn: HTMLButtonElement;
-    public clearAll: HTMLButtonElement;
-    public todos: ITodo[];
+  public addItemText: HTMLInputElement;
+  public addItemBtn: HTMLButtonElement;
+  public clearAll: HTMLButtonElement;
+  public todos: ITodo[];
 
-    private todoList: HTMLDivElement;
+  private todoList: HTMLDivElement;
 
-    constructor() {
-        this.addItemText = document.querySelector(
-            ".task__add"
-        ) as HTMLInputElement;
-        this.addItemBtn = document.getElementById("btn") as HTMLButtonElement;
-        this.todoList = document.querySelector(".todo") as HTMLDivElement;
-        this.clearAll = document.querySelector(".clear") as HTMLButtonElement;
-        this.todos = localStorage.getItem("todoList")
-            ? JSON.parse(localStorage.getItem("todoList")!)
-            : [];
-    }
+  constructor() {
+    this.addItemText = document.querySelector('.task__add') as HTMLInputElement;
+    this.addItemBtn = document.getElementById('btn') as HTMLButtonElement;
+    this.todoList = document.querySelector('.todo') as HTMLDivElement;
+    this.clearAll = document.querySelector('.clear') as HTMLButtonElement;
+    this.todos = localStorage.getItem('todoList')
+      ? JSON.parse(localStorage.getItem('todoList')!)
+      : [];
+  }
 
-    public saveToLocalStorage() {
-        localStorage.setItem("todoList", JSON.stringify(this.todos));
-    }
+  public saveToLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(this.todos));
+  }
 
-    public renderTodoList() {
-        this.todoList.innerHTML = "";
+  public renderTodoList() {
+    this.todoList.innerHTML = '';
 
-        this.todos.forEach((todo) => {
-            if (!this.isExpired(todo.expDate)) {
-                const text = todo.text;
-                const id = todo.id;
+    this.todos.forEach((todo) => {
+      if (!this.isExpired(todo.expDate)) {
+        const text = todo.text;
+        const id = todo.id;
 
-                const todoItem = document.createElement("li");
-                todoItem.classList.add("todo__item");
-                todoItem.setAttribute("data-id", id);
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('todo__item');
+        todoItem.setAttribute('data-id', id);
 
-                const itemInput = document.createElement("input");
-                itemInput.classList.add("todo__text");
-                itemInput.value = text;
-                itemInput.setAttribute("readonly", "readonly");
+        const itemInput = document.createElement('input');
+        itemInput.classList.add('todo__text');
+        itemInput.value = text;
+        itemInput.setAttribute('readonly', 'readonly');
 
-                const itemDiv = document.createElement("div");
+        const itemDiv = document.createElement('div');
 
-                const itemBtnEdit = document.createElement("button");
-                itemBtnEdit.classList.add("edit");
+        const itemBtnEdit = document.createElement('button');
+        itemBtnEdit.classList.add('edit');
 
-                const itemImgEdit = document.createElement("img");
-                itemImgEdit.src = "../img/edit.png";
+        const itemImgEdit = document.createElement('img');
+        itemImgEdit.src = '../img/edit.png';
 
-                const itemBtnDelete = document.createElement("button");
-                itemBtnDelete.classList.add("edit");
+        const itemBtnDelete = document.createElement('button');
+        itemBtnDelete.classList.add('edit');
 
-                const itemImgDelete = document.createElement("img");
-                itemImgDelete.src = "../img/trash-bin.png";
+        const itemImgDelete = document.createElement('img');
+        itemImgDelete.src = '../img/trash-bin.png';
 
-                this.todoList.appendChild(todoItem);
-                todoItem.appendChild(itemInput);
-                todoItem.appendChild(itemDiv);
-                itemDiv.appendChild(itemBtnEdit);
-                itemDiv.appendChild(itemBtnDelete);
-                itemBtnDelete.appendChild(itemImgDelete);
-                itemBtnEdit.appendChild(itemImgEdit);
+        this.todoList.appendChild(todoItem);
+        todoItem.appendChild(itemInput);
+        todoItem.appendChild(itemDiv);
+        itemDiv.appendChild(itemBtnEdit);
+        itemDiv.appendChild(itemBtnDelete);
+        itemBtnDelete.appendChild(itemImgDelete);
+        itemBtnEdit.appendChild(itemImgEdit);
 
-                this.editElement(itemBtnEdit, itemInput, itemImgEdit, todoItem);
-                this.deleteElement(itemBtnDelete, todoItem);
-            } else {
-                let index = this.todos.indexOf(todo);
-                this.todos.splice(index, 1);
-                this.saveToLocalStorage();
-            }
-        });
-    }
-
-    public refreshList() {
+        this.editElement(itemBtnEdit, itemInput, itemImgEdit, todoItem);
+        this.deleteElement(itemBtnDelete, todoItem);
+      } else {
+        let index = this.todos.indexOf(todo);
+        this.todos.splice(index, 1);
         this.saveToLocalStorage();
-        this.renderTodoList();
-    }
+      }
+    });
+  }
 
-    private editElement(
-        button: HTMLButtonElement,
-        input: HTMLInputElement,
-        img: HTMLImageElement,
-        li: HTMLLIElement
-    ) {
-        let switchEditBtnValue = 0;
-        const todo = this.todos.find(
-            (item) => item.id === li.getAttribute("data-id")
-        );
+  public refreshList() {
+    this.saveToLocalStorage();
+    this.renderTodoList();
+  }
 
-        if (todo) {
-            const index = this.todos.indexOf(todo);
-            button.addEventListener("click", (e) => {
-                e.preventDefault();
-                if (switchEditBtnValue === 0) {
-                    input.removeAttribute("readonly");
-                    switchEditBtnValue = 1;
-                    img.src = "../img/check.png";
-                    input.focus();
-                } else if (switchEditBtnValue === 1) {
-                    input.setAttribute("readonly", "readonly");
-                    switchEditBtnValue = 0;
-                    img.src = "../img/edit.png";
-                }
-                if (input.value.trim() === "") {
-                    this.todos.splice(index, 1);
-                    this.refreshList();
-                    return;
-                }
-                this.todos[index].text = input.value;
-                this.saveToLocalStorage();
-            });
+  private editElement(
+    button: HTMLButtonElement,
+    input: HTMLInputElement,
+    img: HTMLImageElement,
+    li: HTMLLIElement
+  ) {
+    let switchEditBtnValue = 0;
+    const todo = this.todos.find(
+      (item) => item.id === li.getAttribute('data-id')
+    );
+
+    if (todo) {
+      const index = this.todos.indexOf(todo);
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (switchEditBtnValue === 0) {
+          input.removeAttribute('readonly');
+          switchEditBtnValue = 1;
+          img.src = '../img/check.png';
+          input.focus();
+        } else if (switchEditBtnValue === 1) {
+          input.setAttribute('readonly', 'readonly');
+          switchEditBtnValue = 0;
+          img.src = '../img/edit.png';
         }
+        if (input.value.trim() === '') {
+          this.todos.splice(index, 1);
+          this.refreshList();
+          return;
+        }
+        this.todos[index].text = input.value;
+        this.saveToLocalStorage();
+      });
     }
+  }
 
-    private deleteElement(button: HTMLButtonElement, li: HTMLLIElement) {
-        button.addEventListener("click", (e) => {
-            this.todos.forEach((task) => {
-                if (task.id === li.getAttribute("data-id")) {
-                    let index = this.todos.indexOf(task);
-                    this.todos.splice(index, 1);
-                    this.refreshList();
-                }
-            });
-        });
-    }
+  private deleteElement(button: HTMLButtonElement, li: HTMLLIElement) {
+    button.addEventListener('click', (e) => {
+      this.todos.forEach((task) => {
+        if (task.id === li.getAttribute('data-id')) {
+          let index = this.todos.indexOf(task);
+          this.todos.splice(index, 1);
+          this.refreshList();
+        }
+      });
+    });
+  }
 
-    private isExpired(expDate: number): boolean {
-        const expirationPeriod = 6 * 60 * 60 * 1000;
-        const currentTime = Date.now();
-        return currentTime - expDate > expirationPeriod;
-    }
+  private isExpired(expDate: number): boolean {
+    const expirationPeriod = 6 * 60 * 60 * 1000;
+    const currentTime = Date.now();
+    return currentTime - expDate > expirationPeriod;
+  }
 }
